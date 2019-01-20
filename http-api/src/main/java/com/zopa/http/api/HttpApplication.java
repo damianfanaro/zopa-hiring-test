3f88789a6636f9ef6275566b103c253b27b410ce
Damian Fanaro <damianfanaro@gmail.com>
2019-01-20 12:54:56 +0100

    Adding RatPack server configuration

--- http-api/src/main/java/com/zopa/http/api/HttpApplication.java
+++ http-api/src/main/java/com/zopa/http/api/HttpApplication.java
@@ -1,12 +1,22 @@
 package com.zopa.http.api;
 
-import com.zopa.GreetingFormatter;
+import ratpack.server.RatpackServer;
+import ratpack.server.RatpackServerSpec;
 
 public class HttpApplication {
-	
-    public static void main(String[] args) {
-        final String output = GreetingFormatter.greeting(args[0]);
-        System.out.println(output);
+
+    public static void main(String[] args) throws Exception {
+        startServer();
+    }
+
+    private static void startServer() throws Exception {
+        RatpackServer.start(HttpApplication::setHandlers);
+    }
+
+    private static RatpackServerSpec setHandlers(RatpackServerSpec server) {
+        return server.handlers(chain -> chain
+                .get(ctx -> ctx.render("Hello World!"))
+                .get(":name", ctx -> ctx.render("Hello " + ctx.getPathTokens().get("name") + "!")));
     }
 
 }
